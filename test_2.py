4c31be50297e773a9d0975b067eb64c2856171f3
vijaykrishnamoorthy <vijaytoinbox@gmail.com>
2019-07-16 11:36:01 +0530

    nothing to edit

--- test_2.py
+++ test_2.py
@@ -1,14 +1,14 @@
 units_used = int(input('Enter the no of units consumed by the customer :'))
 service_tax=1.5
 if(units_used <= 100):
-    bill_amount=units_used*1.5*1
+    bill_amount=units_used*service_tax*1
     print("The Electricity bill amount with TAX is",bill_amount)
 elif(units_used <= 200):
-    bill_amount = units_used*1.5*1.5
+    bill_amount = units_used*service_tax*1.5
     print("The Electricity bill amount with TAX is", bill_amount)
 elif(units_used <= 500):
-    bill_amount = units_used*1.5*3
+    bill_amount = units_used*service_tax*3
     print("The Electricity bill amount with TAX is", bill_amount)
 else:
-    bill_amount = units_used*1.5*5
+    bill_amount = units_used*service_tax*5
     print("The Electricity bill amount with TAX is", bill_amount)
