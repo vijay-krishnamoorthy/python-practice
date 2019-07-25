2028bccb03619b6ac50bdfdba270623f0bfb67a9
vijay-krishnamoorthy <vijaytoinbox@gmail.com>
2019-07-25 16:57:29 +0530

    Daily stuff

--- django-example/example/home/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def home(request):
-    return HttpResponse("<center><h1>Home page</h1></center>")
-
-def about(request):
-    return HttpResponse("<center><h1>About page</h1></center>")
- 
